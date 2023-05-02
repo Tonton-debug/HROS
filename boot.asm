c2ac17ec24f9b341506c2849aa4bd3d403947184
HrUnDeL-sus <pendosfromrussia@gmail.com>
2023-05-02 22:16:37 +0300

    PRINT XXXXXXXXX

--- boot.asm
+++ boot.asm
@@ -1,14 +1,6 @@
+
 [BITS 16]
-mov ah,02h
-mov al,08h
-mov cx,0012h
-mov bx,8100h
-int 13h
-mov sp,810h
-mov ds,100h
+include kernel.asm
 jmp 0000:8100h
-
-
 TIMES 510 - ($ - $$) db 0
 DW 0xAA55
-
--- start_asm.sh
+++ start_asm.sh
@@ -1,3 +1,4 @@
-
-nasm -f bin -o boot.bin boot.asm kernel.asm
-qemu-system-x86_64 -hda boot.bin
+nasm -f bin -o boot.bin boot.asm
+nasm -f bin -o kernel.bin kernel.asm
+cat boot.bin kernel.bin > image.bin
+qemu-system-x86_64 -hda image.bin
